5489fdcb1dc2a42cd912ca5513ab75fcd8c1e890
Kia Tupitsa <kia.tupitsa@student.oulu.fi>
2023-02-28 12:16:20 +0200

    Results-listeners modified

--- resultsListeners.js
+++ resultsListeners.js
@@ -3,18 +3,23 @@ const resultTitles = document.querySelectorAll('.result-title');
 
 // Loop through each result-title element
 resultTitles.forEach(resultTitle => {
-  // Add a click event listener to each result-title element
-  resultTitle.addEventListener('click', () => {
-  // Get the data-result attribute value of the clicked result-title element
-  const resultNumber = resultTitle.getAttribute('data-result');
-  // Get the result-content element corresponding to the clicked result-title element
-  const resultContent = document.querySelector(`.result-content[data-result="${resultNumber}"]`);
+  
+   // Get the data-result attribute value of the current result-title element
+   const resultNumber = resultTitle.getAttribute('data-result');
 
-  // If the result-content element is hidden, show it; otherwise, hide it
-  if (resultContent.style.display === 'none') {
-    resultContent.style.display = 'block';
-  } else {
-    resultContent.style.display = 'none';
-  }
-  });
-});
+   // Get the result-content element corresponding to the current result-title element
+   const resultContent = document.querySelector(`.result-content[data-result="${resultNumber}"]`);
+
+   // Hide the result-content element initially
+   resultContent.style.display = 'none';
+ 
+   // Add a click event listener to each result-title element
+   resultTitle.addEventListener('click', () => {
+     // Toggle the visibility of the result-content element
+     if (resultContent.style.display === 'none') {
+       resultContent.style.display = 'block';
+     } else {
+       resultContent.style.display = 'none';
+     }
+   });
+ });
